6c75c49d51226b53cd3930a99c0b4b656d89afbb
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2025-04-02 12:14:41 +0700

    Uses environment variable for redirect URL
    Updates the redirect URL to use the `FE_REDIRECT_URL`
    environment variable instead of hardcoding it.
    
    This allows for easier configuration of the redirect URL
    in different environments (e.g., development, production).

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -18,7 +18,7 @@ module.exports = {
     const { amount, orderId } = req.body;
     const items = [{}];
     const embed_data = {
-      redirectUrl: "http://localhost:5173/", //fe
+      redirectUrl: FE_REDIRECT_URL, //fe
       // redirectUrl: FE_REDIRECT_URL,
     };
     const transID = Math.floor(Math.random() * 1000000);
